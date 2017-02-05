7037cdf829c1d8527a961096ee4411e8cedbcacd
Christopher Ronning <christopher.a.ronning@gmail.com>
2017-02-04 19:41:06 -0500

    create Example class for initial sloppy testing

---
+++ src/Ferro/Examples.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ferro
+{
+    public class Examples
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Testing one two three");
+
+        }
+    }
+}
