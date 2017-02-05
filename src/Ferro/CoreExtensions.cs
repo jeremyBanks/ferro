6c64fa1caae967ead4a58295c788797730585e8d
Jeremy Banks <_@jeremy.ca>
2017-02-04 22:50:21 -0500

    Generalize MemoryStream.Write to Stream.Write.

--- src/Ferro/CoreExtensions.cs
+++ src/Ferro/CoreExtensions.cs
@@ -3,8 +3,8 @@ using System.IO;
 namespace Ferro {
     // Extensions methods on core types that we use internally.
     public static class CoreExtensions {
-        // Make the offset and length on MemoryStream.Write() optional if you're writing the whole thing.
-        public static void Write(this MemoryStream stream, byte[] bytes) {
+        // Make the offset and length on Stream.Write() optional if you're writing the whole thing.
+        public static void Write(this Stream stream, byte[] bytes) {
             stream.Write(bytes, 0, bytes.Length);
         }
     }
