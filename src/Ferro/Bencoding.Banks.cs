f5e92bef627b40c38e72cf74c8504a6ec5609761
Jeremy Banks <_@jeremy.ca>
2017-02-05 04:57:37 -0500

    Delete src/Ferro/Bencoding.Banks.cs

--- src/Ferro/Bencoding.Banks.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-namespace Bencoding {
-    // A temporary stub of definitions from the other branch, to make some code work in both.
-    class DecodingException : Exception {}
-    class EncodingException : Exception {}
-}
