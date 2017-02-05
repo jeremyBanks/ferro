1c6efb9ceaa97a0b62415ff775a7982a0f84a605
Jeremy Banks <_@jeremy.ca>
2017-02-04 21:30:05 -0500

    Declare Ferro.Bencoding.Banks

---
+++ src/Ferro/Ferro.Bencoding.Banks.cs
@@ -0,0 +1,11 @@
+namespace Ferro  {
+    public static class Bencoding {
+        public static byte[] Encode(object value) {
+            return null;
+        }
+
+        public static object Decode(byte[] bytes) {
+            return new byte[]{};
+        }
+    }
+}
