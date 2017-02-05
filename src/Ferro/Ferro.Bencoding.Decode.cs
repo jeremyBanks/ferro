d8887e8eacc01faea2ea41fcde2b0dc9147eff2a
Jeremy Banks <_@jeremy.ca>
2017-02-04 23:21:46 -0500

    Start implementing Ferro.Bencoding.Decode.

--- src/Ferro/Ferro.Bencoding.Decode.cs
+++ src/Ferro/Ferro.Bencoding.Decode.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
 
 namespace Ferro  {
     public static partial class Bencoding {
@@ -14,8 +16,47 @@ namespace Ferro  {
         }
 
         public static object Decode(Stream input) {
-            // not implemented
-            return null;
+            var firstOrNothing = input.ReadByte();
+            if (firstOrNothing == -1) {
+                throw new DecodingException("Unexpected end of stream.");
+            }
+            var first = (byte) firstOrNothing;
+
+            switch (first) {
+                case (byte) 'i':
+                    return 11;
+                
+                case (byte) '0':
+                    return new byte[]{};
+
+                case (byte) '1':
+                case (byte) '2':
+                case (byte) '3':
+                case (byte) '4':
+                case (byte) '5':
+                case (byte) '6':
+                case (byte) '7':
+                case (byte) '8':
+                case (byte) '9':
+                    return Encoding.ASCII.GetBytes("NOT IMPLEMENTED");
+                
+                case (byte) 'l':
+                    return new List<object> {
+                        Encoding.ASCII.GetBytes("NOT IMPLEMENTED")
+                    };
+                
+                case (byte) 'd':
+                    return new Dictionary<byte[], object> {
+                        {
+                            Encoding.ASCII.GetBytes("NOT IMPLEMENTED"),
+                            Encoding.ASCII.GetBytes("NOT IMPLEMENTED")
+                        }
+                    };
+                
+                default:
+                    throw new DecodingException(
+                        $"Unexpected initial byte in value ${first}.");
+            }
         }
 
         public class DecodingException : Exception {
