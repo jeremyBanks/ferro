439dac1bf2ea330788aeb26aced6e7e3b65465c6
Jeremy Banks <_@jeremy.ca>
2017-02-04 23:04:29 -0500

    Throw an error if there's more data after the first value in Decode(byte[]).

--- src/Ferro/Ferro.Bencoding.Decode.cs
+++ src/Ferro/Ferro.Bencoding.Decode.cs
@@ -4,7 +4,13 @@ using System.IO;
 namespace Ferro  {
     public static partial class Bencoding {
         public static object Decode(byte[] bytes) {
-            return Decode(new MemoryStream(bytes));
+            using (var stream = new MemoryStream(bytes)) {
+                var value = Decode(stream);
+                if (stream.Position < stream.Length) {
+                    throw new DecodingException("Unexpected data after input.");
+                }
+                return value;
+            }
         }
 
         public static object Decode(Stream input) {
