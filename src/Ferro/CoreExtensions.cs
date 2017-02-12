5ee43cc8e11475d3f874231dba5d096c34007bd7
Christopher Ronning <christopher.a.ronning@gmail.com>
2017-02-11 23:55:23 -0500

    rf

--- src/Ferro/CoreExtensions.cs
+++ src/Ferro/CoreExtensions.cs
@@ -67,7 +67,7 @@ namespace Ferro {
             }
 
             byte[] bytes = new byte[hex.Length / 2];
-            for (int i = 0; i < hex.Length; i+=2)
+            for (int i = 0; i < hex.Length; i += 2)
             {
                 bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
             }
