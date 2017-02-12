9d59aeb463e8118421dd65c324dcecdc9872558d
Jeremy Banks <_@jeremy.ca>
2017-02-11 21:35:08 -0500

    Add Ferro.UDPSocket class.
    This wraps the built-in System.Net.Sockets.Socket class to provide a more-friendly less-efficient UDP-Specific interface.

---
+++ src/Ferro/UDPSocket.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Ferro
+{
+    // Dumb async wrapper over the UDP Socket interface.
+    class UDPSocket {
+        readonly IPEndPoint localEndPoint;
+        private Socket dotnetSocket;
+
+        public UDPSocket(IPEndPoint localEndPoint) {
+            this.localEndPoint = localEndPoint;
+            dotnetSocket = new Socket(
+                AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            dotnetSocket.Bind(localEndPoint);
+        }
+
+        public void SendTo(byte[] data, IPEndPoint remoteEndPoint) {
+            var sent = dotnetSocket.SendTo(data, remoteEndPoint);
+            if (sent != data.Length) {
+                throw new Exception($"Failed to send all data to {remoteEndPoint}.");
+            }
+        }
+
+        public Task<ReceivedMessage> Receive() {
+            var taskSource = new TaskCompletionSource<ReceivedMessage>();
+            
+            byte[] buffer = new byte[65507];
+            // over-sized, but what can you do?
+
+            SocketAsyncEventArgs responseHandling = new SocketAsyncEventArgs();
+            responseHandling.SetBuffer(buffer, 0, buffer.Length);
+            responseHandling.RemoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            
+            Action onReceived = () => {
+                if (responseHandling.SocketError != SocketError.Success) {
+                    throw new Exception($"Got socket error {responseHandling.SocketError}");
+                }
+
+                var data = new byte[responseHandling.BytesTransferred];
+                Array.Copy(buffer, 0, data, 0, responseHandling.BytesTransferred);
+                
+                taskSource.SetResult(new ReceivedMessage {
+                    Data = data,
+                    Source = (IPEndPoint) responseHandling.RemoteEndPoint
+                });
+            };
+
+            responseHandling.Completed += (_1, _2) => onReceived();
+
+            var willRaiseEvent = dotnetSocket.ReceiveFromAsync(responseHandling);
+            if (!willRaiseEvent) {
+                onReceived();
+            }
+
+            return taskSource.Task;
+        }
+
+        public class ReceivedMessage {
+            public byte[] Data;            
+            public IPEndPoint Source;
+        }
+    }
+}
