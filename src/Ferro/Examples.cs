6f18baf8a3ffd42ec1445eaec09ba8bc616dbb64
Jeremy Banks <_@jeremy.ca>
2017-02-11 22:09:30 -0500

    Split out example methods in Examples.

--- src/Ferro/Examples.cs
+++ src/Ferro/Examples.cs
@@ -3,8 +3,17 @@
     class Examples {
         public static void Main()
         {
+            dhtClient();
+            tcpPeerProtocol();
+        }
+
+        static void dhtClient() {
             var dht = new DHTClient();
             dht.Ping().Wait();
+        }
+
+        static void tcpPeerProtocol() {
+            // TODO
         }
     }
 }
