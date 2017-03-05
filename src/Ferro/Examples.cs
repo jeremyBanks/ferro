1148b2a63494190120995647256b54a87228115b
Jeremy Banks <_@jeremy.ca>
2017-03-05 14:27:47 -0500

    Clean-up of existing DHTClient.
    DHTClient and UDPSocket are now Disposable, with Finalizers.
    
    DHTClient's example is going to try to get peers for the Ubuntu torrent. The test peer won't be able to provide those directly, so that will require successful use of the DHT.

--- src/Ferro/Examples.cs
+++ src/Ferro/Examples.cs
@@ -5,6 +5,12 @@ using System.Threading.Tasks;
 namespace Ferro
 {
     class Examples {
+        // Torrents we expect to be loaded into our test peer.
+        readonly static byte[] veryTinyKnownInfohash = "ea45080eab61ab465f647e6366f775bf25f69a61".FromHex();
+        readonly static byte[] lessTinyKnownInfohash = "68d22f0f856ca5056e009ac53597a66c0cb03068".FromHex();
+        // Torrents we do not expect to be loaded in our test peer, but which should have many peers online.
+        readonly static byte[] ubuntuUnknownInfohash = "34930674ef3bb9317fb5f263cca830f52685235b".FromHex();
+
         public static int Main(string[] args)
         {
             var testAddress = IPAddress.Loopback;
@@ -19,34 +25,35 @@ namespace Ferro
             dhtClient(testAddress).Wait();
             Console.Write("\n");
             tcpPeerProtocol(testAddress);
+            
 
             return 0;
         }
 
         static async Task dhtClient(IPAddress testAddress) {
-            var dht = new DHTClient();
+            using (var dht = new DHTClient()) {
+                var testNode = new IPEndPoint(testAddress, 9527);
+                var testNodeId = await dht.Ping(testNode);
 
-            var testNode = new IPEndPoint(testAddress, 9527);
-            var testNodeId = await dht.Ping(testNode);
+                Console.WriteLine(
+                    $"Successfully pinged {testNode} and got response with node ID {testNodeId.ToHex()}");
+                
+                var peers = await dht.GetPeers(veryTinyKnownInfohash);
 
-            Console.WriteLine(
-                $"Successfully pinged {testNode} and got response with node ID {testNodeId.ToHex()}");
-            
-            var infoHash = "ea45080eae6eab465f647e6366f775bf25f69a61".FromHex();
+                Console.WriteLine(
+                    $"Requested peers for {veryTinyKnownInfohash.ToHex()} (known torrent) and got some response!");
+                
+                var ubuntuPeers = await dht.GetPeers(ubuntuUnknownInfohash);
 
-            var peers = await dht.GetPeers(infoHash);
-
-            Console.WriteLine(
-                $"Requested peers for {infoHash.ToHex()} and got some response!");
-            
+                Console.WriteLine(
+                    $"Requested peers for {ubuntuUnknownInfohash.ToHex()} (unknown torrent) and got some response!");
+            }
         }
 
         static void tcpPeerProtocol(IPAddress testAddress) {
             var connector = new PeerConnection(IPAddress.Any);
-            var infoHash = "ea45080eab61ab465f647e6366f775bf25f69a61".FromHex();
-            var infoHashMultiPiece = "68d22f0f856ca5056e009ac53597a66c0cb03068".FromHex();
-            connector.InitiateHandshake(testAddress, 45566, infoHash);
-            connector.InitiateHandshake(testAddress, 45566, infoHashMultiPiece);
+            connector.InitiateHandshake(testAddress, 45566, veryTinyKnownInfohash);
+            connector.InitiateHandshake(testAddress, 45566, lessTinyKnownInfohash);
             Console.WriteLine("Finished with Handshake");
         }
         
