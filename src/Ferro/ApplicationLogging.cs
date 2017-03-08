7b79d011251b3617ae194edc36be25ff7a71cceb
Christopher Ronning <christopher.a.ronning@gmail.com>
2017-03-07 21:42:43 -0500

    import Microsoft.Extensions.Logging; create static logger

---
+++ src/Ferro/ApplicationLogging.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+namespace Ferro
+{
+    public class ApplicationLogging
+    {
+        public static ILoggerFactory LoggerFactory { get; } = new LoggerFactory();
+        public static ILogger CreateLogger<T>() => LoggerFactory.CreateLogger<T>();
+    }
+}
